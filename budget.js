ca16921a8a832b3077f7b8bc11a05ee1352ad2f1
charles-large <70664028+charles-large@users.noreply.github.com>
2020-11-19 22:17:00 -0500

    Create budget.js

---
+++ budget.js
@@ -0,0 +1,98 @@
+exports.handler = async (event) => {
+    console.log(event)
+    // variables from user input
+    var purpose = event.currentIntent.slots.Purpose.toLowerCase();
+    var budget = parseInt(event.currentIntent.slots.Budget_Amount.replace(/\D/g,''));
+    var games = event.currentIntent.slots.games_played
+    // if user selects gaming as a purpose, and games has not yet been specified, will ask them for it
+    if (purpose.includes("gaming")) {
+        if (games == null) {
+        let lambda_response = {     
+            "dialogAction": {     
+                "type": "ElicitSlot",
+        
+            "message": {       
+                "contentType": "PlainText",
+                "content": "Your budget of " + budget + " has been confirmed \
+                \
+                What kind of games do you play?"
+        },
+        "intentName": "Budget",
+        "slots": {
+            "Budget_Amount": budget,
+            "Purpose": purpose,
+            // "games_played": null
+        },
+        "slotToElicit": "games_played"
+        
+     } 
+    
+    }
+    return lambda_response;
+    }
+    // If games has a value, then print statement and move on
+    else if (games != null){
+    let lambda_response = {     
+            "dialogAction": {     
+                "type": "ElicitSlot",
+            "slotToElicit": "monitor_resolution",
+            "intentName": "Budget",
+            "slots": {},
+            "responseCard": {
+                "genericAttachments": [
+                    { 
+                        "buttons": [
+                            {
+                                "text": "1920x1080",
+                                "value": "1080"
+                            },
+                            {
+                                "text": "2560x1440",
+                                "value": "2K"
+                            },
+                            {
+                                "text": "3840x2160",
+                                "value": "4K"
+                            },
+                            {
+                                "text": "Not sure",
+                                "value": "1080"
+                            }
+                        ],
+                        "subTitle": "What resolution monitor do you play at?",
+                        "title": "Monitor Resolution"
+                    }
+                    ],
+                    "version": 1,
+                    "contentType": "application/vnd.amazonaws.card.generic"
+                    
+                            }
+                            }
+        
+                    } 
+     return lambda_response;  
+    }
+    }
+    
+    
+    // If user selects streaming as a purpose
+    else if (event.currentIntent.slots.Purpose == "Streaming") {
+        let lambda_response = {     
+            "sessionAttributes": {
+                "budget":  event.currentIntent.slots.Budget_Amount,
+        },   
+            "dialogAction": {     
+                "type": "Close",
+                "fulfillmentState": "Fulfilled",
+        
+            "message": {       
+                "contentType": "PlainText",
+                "content": "Your budget of " + event.currentIntent.slots.Budget_Amount + " has been confirmed"
+        },
+     } 
+    }
+    return lambda_response;
+    }
+    
+    
+};
