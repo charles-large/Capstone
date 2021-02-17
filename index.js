38ba65469416eb8e1ffddee13a447cb275b2c4ae
charles-large <clarge67@gmail.com>
2021-02-17 17:23:24 -0500

    Compared

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ exports.handler = function (event, context, callback){
                                     "fulfillmentState": "Fulfilled",
                                     "message": {
                                       "contentType": "PlainText",
-                                      "content": (apple(data).toString
+                                      "content": (apple(data).toString())
                                     }
                             }
                         }
