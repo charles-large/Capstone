3cc38e5ca19550a45d8ef6784b6f0199e0d9768a
charles-large <clarge67@gmail.com>
2021-02-17 12:51:45 -0500

    test23 query syntax

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function getResult(game_choice){
             data = []
             var sql = "SELECT * FROM pc_games.steam_games WHERE name LIKE 'Ag%' AND minimum_requirements NOT IN ('','NaN') AND types = 'app'";
             connection.query(sql, [game_choice], function(err, rows, fields){
-                rows = rows.map(v => Object.assign({}, v));
-                fields = fields.map(v => Object.assign({}, v));
+                result = JSON.stringify(rows);
                 if (err) {
                     return reject(err);
                 }
@@ -27,7 +26,7 @@ function getResult(game_choice){
                 //console.log(fields[0].minimum_requirements)
                 //results = JSON.stringify(rows);
                 
-                resolve(rows,fields);
+                resolve(result);
             });
             connection.end()
             });
@@ -111,7 +110,7 @@ exports.handler = function (event, context, callback){
     else if (resolution != null) {
         // code here to RDS database
         const game_choice = event.currentIntent.slots.games_played;
-        getResult(game_choice).then(function(rows,fields) {
+        getResult(game_choice).then(function(result) {
             //Parse response from database
             //console.log("test sample " + rows[0][1])
             //console.log("Minimum require " + rows[0][0].minimum_requirements)
@@ -141,7 +140,7 @@ exports.handler = function (event, context, callback){
                         "fulfillmentState": "Fulfilled",
                         "message": {
                           "contentType": "PlainText",
-                          "content": "Rows: " + rows + "Fields" + fields
+                          "content": "Rows: " + result
                         }
                 }
             }
