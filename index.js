43abbbdbf5ce6078736fe8d954e10682a26fd67f
charles-large <clarge67@gmail.com>
2021-02-17 11:55:32 -0500

    test14 query syntax

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ function getResult(game_choice){
                 //console.log(rows)
                 //console.log(fields)
                 //console.log(fields[0].minimum_requirements)
-                list = rows.forEach(function(row) {
-                    console.log(row.name)
+                results = []
+                rows.forEach(function(row) {
+                    results.push(row);
                 })
-                resolve(list);
+                resolve(results);
             });
             connection.end()
             });
