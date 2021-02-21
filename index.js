6033dee1ac24ab03093f0a26d88e4c65b1fa9a19
charles-large <clarge67@gmail.com>
2021-02-21 15:45:16 -0500

    Fixed code, added promises and async functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 var mysql = require('mysql');
 
+
+exports.handler = function (event, context, callback){
+
 var connection = mysql.createConnection({
     host: process.env.db_host,
     user: process.env.db_user,
@@ -10,38 +13,105 @@ var connection = mysql.createConnection({
 
 function getResult(game_choice){
     return new Promise((resolve, reject) => {
-    //connection.connect();
-    //const game_choice = "Battle Brothers";
-    let sql = "SELECT * FROM pc_games.steam_games WHERE name LIKE ?";
-    connection.query(sql, [game_choice], (err, rows, fields) => {
-        if (err || rows === ''){
-            let sql = "SELECT * FROM pc_games.steam_games WHERE name LIKE CONCAT(?,'%') AND minimum_requirements NOT IN ('','NaN') AND types = 'app' LIMIT 5";
-            connection.query(sql, [game_choice], (err, rows, fields) => {
-                if (err){
-                    console.log(err)
-                }
+        const sql = "SELECT * FROM pc_games.steam_games WHERE name LIKE ?";
+        connection.query(sql, [game_choice], (err, rows, fields) => {
+            if (rows.length > 0){
+                console.log(rows)
+                console.log("This is being executed")
                 resolve(rows)
-                
-
-            }) 
-        }
-        resolve(rows);
+            }
+            if (err){
+                console.log(err)
+            }
+            else{
+                console.log("This else is being executed")
+                const sql2 = "SELECT * FROM pc_games.steam_games WHERE name LIKE CONCAT(?,'%') AND minimum_requirements NOT IN ('','NaN') AND types = 'app' LIMIT 5";
+                connection.query(sql2, [game_choice], (err, rows, fields) => {
+                if (!err && !false){
+                    resolve(rows)
+                }
+                else{
+                    reject(console.log(err))
+                }
+            })
+            }
+            connection.end()
+        })
         
-    });
-    connection.end(err => console.log(err))
-    });
+        })
+
 }
 
 
-exports.handler = function (event, context, callback){
-    console.log(event);
-    // variables from user input
-    var purpose = event.currentIntent.slots.Purpose.toLowerCase();
-    var budget = parseInt(event.currentIntent.slots.Budget_Amount.replace(/\D/g,''));
-    var games = event.currentIntent.slots.games_played;
-    var resolution = event.currentIntent.slots.monitor_resolution;
-    // if user selects gaming as a purpose, and games has not yet been specified, will ask them for it
-    if (purpose.includes("gaming")) {
+
+function getSecondResult(rows) {
+                        const data = []
+                        for (var i = 0; i < rows.length; i++){
+                        //Parse response from database
+                        const query = rows[i].minimum_requirements
+                        // Removes semi colon and commas from output
+                        const rows_parsed = query.replace(/[:,]/g,' ');
+                        //Removes excessive white space
+                        const rows_parsed2 = rows_parsed.replace(/\s+/g,' ').trim()
+                        //Seperates output and only includes sections that Begin with Processor and end with Storage
+                        var part = rows_parsed2.substring(
+                        rows_parsed2.lastIndexOf(" Processor") + 1, 
+                        rows_parsed2.lastIndexOf("Storage")
+                        );
+                        //Replaces the three categories with a semi colon for visibility
+                        var rows_parsed3 = part.replace(/Processor/g,"Processor:").replace(/Graphics/g,",Graphics:").replace(/Memory/g,",Memory:")
+                        // Splits the output into three sections by a comma representing the three categories
+                        var rows_parsed4 = rows_parsed3.split(',');
+                        data.push(rows_parsed4)
+                        }
+                        const apple = function (x){
+                            const string_database = []
+                            for (var i = 0; i < data.length; i++){
+                            const statement = "\n"+ "Name: " + rows[i].name + "\n" + " Minimum Requirements: " + "\n" + data[i][0] + "\n" + data[i][1] + "\n" + data[i][2] + "\n"
+                            string_database.push(statement)
+                        }
+                        return(string_database)
+                        }
+            
+                //
+                let lambda_response = {
+                            "dialogAction": {
+                                    "type": "Close",
+                                    "fulfillmentState": "Fulfilled",
+                                    "message": {
+                                      "contentType": "PlainText",
+                                      "content": "Steam Requirements:" + "\n" + "\n" + (apple(data).toString()) + "\n"
+                                    }
+                            }
+                        }
+                        console.log(lambda_response)
+                        callback(null,lambda_response)
+                
+            }
+
+
+
+async function doSomething(game_choice) {
+    try{
+        const first = await getResult(game_choice)
+        console.log("first done")
+        const second = await getSecondResult(first)
+        console.log("second done")
+        return(second)
+    }
+    catch (err) {
+        console.log("Error")
+    }
+}
+
+var purpose = event.currentIntent.slots.Purpose.toLowerCase();
+var budget = parseInt(event.currentIntent.slots.Budget_Amount.replace(/\D/g,''));
+var games = event.currentIntent.slots.games_played;
+var resolution = event.currentIntent.slots.monitor_resolution;
+
+
+
+if (purpose.includes("gaming")) {
         if (games == null) {
         let lambda_response = {     
             "dialogAction": {     
@@ -106,67 +176,19 @@ exports.handler = function (event, context, callback){
         
                     };
      callback(null,lambda_response);  
-    }
-    else if (resolution != null) {
-            const game_choice = event.currentIntent.slots.games_played;
-            getResult(game_choice).then(function(rows) {
-                        const data = []
-                        for (var i = 0; i < rows.length; i++){
-                        //Parse response from database
-                        const query = rows[i].minimum_requirements
-                // Removes semi colon and commas from output
-                const rows_parsed = query.replace(/[:,]/g,' ');
-                //Removes excessive white space
-                const rows_parsed2 = rows_parsed.replace(/\s+/g,' ').trim()
-                //Seperates output and only includes sections that Begin with Processor and end with Storage
-                var part = rows_parsed2.substring(
-                rows_parsed2.lastIndexOf(" Processor") + 1, 
-                rows_parsed2.lastIndexOf("Storage")
-            );
-                //Replaces the three categories with a semi colon for visibility
-                var rows_parsed3 = part.replace(/Processor/g,"Processor:").replace(/Graphics/g,",Graphics:").replace(/Memory/g,",Memory:")
-                // Splits the output into three sections by a comma representing the three categories
-                var rows_parsed4 = rows_parsed3.split(',');
-                data.push(rows_parsed4)
-                        }
-                        const apple = function (x){
-                            const string_database = []
-                            for (var i = 0; i < data.length; i++){
-                            const statement = "\n"+ "Name: " + rows[i].name + "\n" + " Minimum Requirements: " + "\n" + data[i][0] + "\n" + data[i][1] + "\n" + data[i][2] + "\n"
-                            string_database.push(statement)
-                        }
-                        return(string_database)
-                        }
-            
-                //
-                let lambda_response = {
-                            "dialogAction": {
-                                    "type": "Close",
-                                    "fulfillmentState": "Fulfilled",
-                                    "message": {
-                                      "contentType": "PlainText",
-                                      "content": "Steam Requirements:" + "\n" + "\n" + (apple(data).toString()) + "\n"
-                                    }
-                            }
-                        }
-                        console.log(lambda_response)
-                        callback(null,lambda_response)
-                
-            })
-            
-            
-
-        
+        }
     
 
-        
+
     
-    }
-    }
-    }
-    
-    
-    // If user selects streaming as a purpose
+else if (resolution != null) {    
+const game_choice = event.currentIntent.slots.games_played;
+doSomething(game_choice)
+}
+}
+
+
+ // If user selects streaming as a purpose
     else if (event.currentIntent.slots.Purpose == "Streaming") {
         let lambda_response = {     
             "sessionAttributes": {
@@ -184,6 +206,8 @@ exports.handler = function (event, context, callback){
     };
     return lambda_response;
     }
+}
     
     
-};
+    
+}
