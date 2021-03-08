3c323e063cce5dd71d4c685c1beceae3816c84fb
charles-large <clarge67@gmail.com>
2021-03-07 22:40:41 -0500

    reset code

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-const data = ["test1"]
-name_database=[]
+var aws = require('aws-sdk');
+var lambda = new aws.Lambda({
+    region: 'us-east-1'
+});
 
 
 
-console.log(data.length)
+
+exports.handler = function (event, context, callback){
+const test = lambda.invoke({
+    FunctionName: 'arn:aws:lambda:us-east-1:742033175622:function:pcbot_price',
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify(event, null, 2)
+}, function(error, data){
+    if (error){
+        context.done('error', error)
+    }
+    if(data.Payload){
+        const stuff = data.Payload
+    }
+}
+)
+}
